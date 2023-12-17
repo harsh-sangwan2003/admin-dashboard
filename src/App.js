06c9bccb5e7100ba2f05d0257e7f1bfbebffedf7
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-17 10:39:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Home from "./pages/home/Home";
 import Login from './pages/login/Login';
-import List from './pages/login'
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,9 +11,20 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+
           <Route path="/">
-            <Route index element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
           </Route>
         </Routes>
       </Router>
