e49f321c37822dcdde5d26357b7301359bdb7cfa
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-17 11:12:15 +0530

    added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
 import './Sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+
+      <div className="top">
+        <span className='logo'>admindashbaord</span>
+      </div>
+
+      <hr />
+
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className='icon' />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonOutlineOutlinedIcon className='icon' />
+            <span>Users</span>
+          </li>
+          <li>
+            <Inventory2OutlinedIcon className='icon' />
+            <span>Products</span>
+          </li>
+          <li>
+            <ViewListIcon className='icon' />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className='icon' />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <SignalCellularAltIcon className='icon' />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className='icon' />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <MonitorHeartIcon className='icon' />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className='icon' />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className='icon' />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className='icon' />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className='icon' />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
     return (
         <div className='home'>
-            Home
+            <Sidebar/>
+            <div className="homeContainer">
+                Container
+            </div>
         </div>
     )
 }
