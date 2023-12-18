55b237f29684989878ffe4305974ee169070f7b1
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-18 18:43:28 +0530

    added

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './List.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Datatable from '../../components/datatable/Datatable'
 
 const List = () => {
   return (
     <div className='list'>
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
-        datatable
+        <Navbar />
+        <Datatable/>
       </div>
     </div>
   )
