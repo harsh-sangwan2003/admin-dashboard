0704b4c10af1a30ef2dc59178e2b76ef3e1c167c
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-18 15:55:52 +0530

    added

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -78,12 +78,19 @@ const List = () => {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row">{row.id}</TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} className="image" />
+                </div>
+              </TableCell>
               <TableCell className="tableCell">{row.product}</TableCell>
               <TableCell className="tableCell">{row.customer}</TableCell>
               <TableCell className="tableCell">{row.date}</TableCell>
               <TableCell className="tableCell">{row.amount}</TableCell>
               <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">{row.status}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
